98ea336042021ea11d2621179f83e1b57beaab73
Carlos Soares <carlos.soares@speortle.com>
2026-03-19 20:01:18 -0300

    Inclusão de Processes

---
+++ Model/Dto/ProcessDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CEJ_WebApp.Model.Dto
+{
+    public class ProcessDto
+    {
+        public string ProcessNumber { get; set; }
+        public string Status { get; set; }
+        public string Client { get; set; }
+        public DateTime OpenDate { get; set; }
+        public DateTime LastMovimentationDate { get; set; }
+    }
+}
