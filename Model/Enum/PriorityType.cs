5df420240d2c8e933d39b15072a802e5636c402d
Carlos Soares <carlos.soares@speortle.com>
2026-03-23 12:54:16 -0300

    backup do meio do dia

---
+++ Model/Enum/PriorityType.cs
@@ -0,0 +1,9 @@
+﻿namespace CEJ_WebApp.Model.Enum
+{
+    public enum PriorityType
+    {
+        Alta =1,
+        Media =2,
+        Baixa=3
+    }
+}
---
+++ Model/Exemplo/ProcessoModel.cs
@@ -0,0 +1,22 @@
+﻿using CEJ_WebApp.Model.Enum;
+using System.ComponentModel.DataAnnotations;
+
+namespace CEJ_WebApp.Model.Exemplo
+{
+    public class ProcessoModel
+    {
+        [Required]
+        public string NumeroProcesso { get; set; }
+
+        public string TipoAcao { get; set; }
+        public PriorityType Prioridade { get; set; } = PriorityType.Baixa;
+        public string TipoAcaoDescricao { get; set; }
+        public string Status { get; set; }
+        public DateTime? DataAbertura { get; set; }
+        public string VaraComarca { get; set; }
+        public string VaraComarca2 { get; set; }
+        public DateTime? PrazoProcessual { get; set; }
+        public bool AtivarAlerta { get; set; }
+
+    }
+}
