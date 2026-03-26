c4b7ecb1c57f9cd18c77236d9d6bf9da611c3369
Carlos Soares <carlos.soares@speortle.com>
2026-03-25 21:32:32 -0300

    Ajuste erro na inserção

--- Model/ClientEntity.cs
+++ Model/ClientEntity.cs
@@ -27,7 +27,7 @@ namespace CEJ_WebApp.Model
         public string? PhoneNumber { get; set; } = null!;
 
         [MaxLength(50, ErrorMessage = "O nome deve ter no máximo 50 caracteres.")]
-        public string? ContactName { get; set; }
+        public string? ContactName { get; set; } = null!;
 
         public long AddressId { get; set; } = 0;
 
@@ -35,7 +35,7 @@ namespace CEJ_WebApp.Model
         public string? GeneralRegister { get; set; }
 
         [MaxLength(20, ErrorMessage = "A profissao deve ter no máximo 20 caracteres.")]
-        public string? Profession { get; set; }
+        public string Profession { get; set; } =  string.Empty;
 
         public AddressEntity Address { get; set; } = new AddressEntity();
         public bool Active { get; set; } = false;
