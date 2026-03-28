2be77ead02d64763675de2dc1ccc47a7765be704
Carlos Soares <carlos.soares@speortle.com>
2026-03-28 11:02:44 -0300

    Final do dia.

---
+++ Model/PartyLegalProcessEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace CEJ_WebApp.Model
+{
+    public class PartyLegalProcessEntity
+    {
+        public string Tipo { get; set; } = "Autor";
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Profissao { get; set; }
+        public string RG { get; set; }
+        public bool Desconhecido { get; set; }
+        public bool ExistenciaUniaoEstavel { get; set; }
+    }
+}
